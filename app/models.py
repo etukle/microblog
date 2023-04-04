e893826a43f5ef5b13c03b6c2b92e9b833c5a313
Evren Tukle <tukleevren@gmail.com>
2023-04-04 14:17:40 +0100

    Forgot password page and reset password mail support added.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from hashlib import md5
+from time import time
+
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db, login
+from app import db, login, app
 
 followers = db.Table(
     'followers',
@@ -56,6 +59,20 @@ class User(UserMixin, db.Model):
         )
 
         return followed.union(self.posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            user_id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(user_id)
+
 
 @login.user_loader
 def load_user(id):
